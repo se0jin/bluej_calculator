6952e5b8cda5ac781019839a78c55743559f5ce2
seoyun211 <seoyun_j211@naver.com>
2026-05-05 14:01:10 +0900

    객체 입력 추가 및 if문으로 student정리

--- MyApp.java
+++ MyApp.java
@@ -1,36 +1,39 @@
-import java.util.Scanner;
 /**
- * MyApp 실행 클래스
+ * Main 클래스
+ * 학생 객체 4개를 생성하고 사용자 입력으로 구구단을 출력하는 클래스
  *
  * @author (2팀)
  * @version (2026.05.05)
  */
-public class MyApp
-{
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        
-        Student jenna = new Student("Jenna", "12345");
-        Student john = new Student("John", "12346");
-        Student maria = new Student("Maria", "12347");
-        Student james = new Student("James", "12348");
-        
-        System.out.print("Jenna에게 질문할 단을 입력하세요>> ");
-        int dan1 = scan.nextInt();
-        jenna.answerGugudan(dan1); 
+import java.util.Scanner;
 
-        System.out.print("John에게 질문할 단을 입력하세요>> ");
-        int dan2 = scan.nextInt();
-        john.answerGugudan(dan2);
+public class MyApp {
+    public static void main(String[] args) {
+        // Student 객체 4개 생성 (이름, 학번)
+        Student jenna = new Student("Jenna", "2401");
+        Student mike  = new Student("Mike",  "2402");
+        Student sara  = new Student("Sara",  "2403");
+        Student tom   = new Student("Tom",   "2404");
 
-        System.out.print("Maria에게 질문할 단을 입력하세요>> ");
-        int dan3 = scan.nextInt();
-        maria.answerGugudan(dan3);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("James에게 질문할 단을 입력하세요>> ");
-        int dan4 = scan.nextInt();
-        james.answerGugudan(dan4);
-        
-        scan.close();
+        System.out.print("누구에게 물어볼까요? (Jenna/Mike/Sara/Tom): ");
+        String name = sc.nextLine();
+
+        System.out.print("몇단을 물어볼까요?");
+        int dan = sc.nextInt();
+
+        // 입력한 이름에 해당하는 학생 객체가 구구단 답하기
+        if (name.equals("Jenna")) {
+            jenna.answerGugudan(dan);
+        } else if (name.equals("Mike")) {
+            mike.answerGugudan(dan);
+        } else if (name.equals("Sara")) {
+            sara.answerGugudan(dan);
+        } else if (name.equals("Tom")) {
+            tom.answerGugudan(dan);
+        } else {
+            System.out.println("없는 학생이에요!");
+        }
     }
 }
--- Student.java
+++ Student.java
@@ -5,13 +5,14 @@
  * @version (2026.05.05)
  */
 public class Student {
-    private String name;      
-    private String studentId; 
+    private String name;
+    private String studentId;
 
     public Student(String name, String studentId) {
         this.name = name;
         this.studentId = studentId;
     }
+
     public void answerGugudan(int dan) {
         for (int i = 1; i <= 9; i++) {
             System.out.print(dan + "x" + i + "=" + (dan * i));

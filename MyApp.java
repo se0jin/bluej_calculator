1b19e1d49c72c60ed16ea3f8f1ba2430671a3362
seoyun211 <seoyun_j211@naver.com>
2026-05-05 16:20:29 +0900

    수정

--- MyApp.java
+++ MyApp.java
@@ -1,5 +1,5 @@
 /**
- * Main 클래스
+ * MyApp 클래스
  * 학생 객체 4개를 생성하고 사용자 입력으로 구구단을 출력하는 클래스
  *
  * @author (2팀)
@@ -16,10 +16,8 @@ public class MyApp {
         Student tom   = new Student("Tom",   "2404");
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("누구에게 물어볼까요? (Jenna/Mike/Sara/Tom): ");
         String name = sc.nextLine();
-
         System.out.print("몇단을 물어볼까요?");
         int dan = sc.nextInt();
 
